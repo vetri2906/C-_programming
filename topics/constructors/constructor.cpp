c304d4caa0ac791eb3c2f5fb8b1f6bcee17e1619
vetri <vetri290696@gmail.com>
2024-09-21 00:28:17 +0530

    added topics folder

---
+++ topics/constructors/constructor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class employee_details
+{
+    public:
+    int age;
+    string role;
+    employee_details (int x, string y)
+    {
+        age = x;
+        role = y;
+    }
+};
+
+int main ()
+{
+    employee_details ram(28, "software engineer");
+    cout << "my age is : " << ram.age << "\nmy designation is " << ram.role<<"\n";
+}
